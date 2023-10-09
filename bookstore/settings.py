816d62ae1de4d8dc300f35fc1db8429873ebf82c
Jose Lima <jalimag0206@gmail.com>
2023-10-09 19:52:27 -0300

    create serializers and tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    "debug_toolbar",
+    "rest_framework.authtoken",
+    'order',
+    'product',
 ]
 
 MIDDLEWARE = [
