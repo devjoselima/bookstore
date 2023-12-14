db8277e48be68e0939227b3724588ae69f8b2005
Jose Lima <jalimag0206@gmail.com>
2023-12-14 12:49:40 -0300

    fix: remove auth from order

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -6,7 +6,5 @@ from order.models import Order
 from order.serializers import OrderSerializer
 
 class OrderViewSet(ModelViewSet):
-  authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-  permission_classes = [IsAuthenticated]
   serializer_class = OrderSerializer
   queryset = Order.objects.all().order_by('id')
