d020d2cd60ce70d33664ba675f6e4b2dbb7725b5
Jose Lima <jalimag0206@gmail.com>
2023-12-24 23:25:33 -0300

    fix: fix debug toolbar

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
-    path('admin/', admin.site.urls),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
